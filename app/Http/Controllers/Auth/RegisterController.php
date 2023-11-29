9aee2aac9ef0e1955df26ec10d527c4a9feba58a
gbzzz <gbp3@aluno.ifal.edu.br>
2023-11-29 15:09:49 -0300

    add: exception de email já utilizado

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,12 +8,18 @@ use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\User\UserResource;
 use App\Models\User;
 use Illuminate\Support\Str;
+use App\Exceptions\UserHasBeenTakenException;
 
 class RegisterController extends Controller
 {
     public function __invoke(RegisterRequest $request)
     {
         $input = $request->validated();
+
+        if (User::query()->whereEmail($input['email'])->exists()) {
+            throw new UserHasBeenTakenException();
+        }
+
         $input['password'] = bcrypt($input['password']);
         $input['token'] = Str::uuid();
         $user = User::query()->create($input);
